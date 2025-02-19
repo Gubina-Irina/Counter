d540ebf1c925ca4df6d20b4e4396c40d768f1561
Gubina Irina <banshchikova.i.s@gmail.com>
2025-02-20 01:30:12 +0700

    Добавлены изменения после рекомендаций от ревьювера

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,32 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var zeroingButton: UIButton!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var historyOfChanges: UITextView!
-    @IBOutlet weak var resultCounter: UILabel!
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var zeroingButton: UIButton!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var historyOfChanges: UITextView!
+    @IBOutlet private weak var resultCounter: UILabel!
     private var counterValue: Int = 0
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         resultCounter.text = "\(counterValue)"
         historyOfChanges.text = "История изменений:\n\n"
-
-        // Do any additional setup after loading the view.
+        
     }
-
+    
     @IBAction func counterDecreaseButton(_ sender: Any) {
         if counterValue > 0  {
-                   counterValue -= 1
-                   resultCounter.text = "\(counterValue)"
-                   addToHistory("значение изменено на -1")
-               }
-               else {
-                   addToHistory("попытка уменьшить значение счётчика ниже 0")
-               }
+            counterValue -= 1
+            resultCounter.text = "\(counterValue)"
+            addToHistory("значение изменено на -1")
+        }
+        else {
+            addToHistory("попытка уменьшить значение счётчика ниже 0")
+        }
     }
     
     @IBAction func counterIncreaseButton(_ sender: Any) {
@@ -42,18 +41,18 @@ class ViewController: UIViewController {
     }
     @IBAction func zeroingCounterButton(_ sender: Any) {
         counterValue = 0
-                resultCounter.text = "\(counterValue)"
-                addToHistory("значение сброшено")
-
+        resultCounter.text = "\(counterValue)"
+        addToHistory("значение сброшено")
+        
     }
     private func addToHistory(_ text: String){
-           let dateFormatter = DateFormatter()
-           dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
-           let timestamp = dateFormatter.string(from: Date())
-           let newEntry = "[\(timestamp)]: \(text)\n"
-           historyOfChanges.text.append(newEntry)
-       }
-
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let timestamp = dateFormatter.string(from: Date())
+        let newEntry = "[\(timestamp)]: \(text)\n"
+        historyOfChanges.text.append(newEntry)
+    }
+    
     
 }
 
